26310c3f0e00ae46d468af4263d4f4d8c644833b
hanyoonseok <hys6078@naver.com>
2021-11-27 13:30:33 +0900

    feat: 사용자로부터 자동차 이름 입력 추가

--- src/index.js
+++ src/index.js
@@ -5,6 +5,40 @@ export default class RacingGame {
     this.$carName = document.getElementById('carName');
     this.$tryCount = document.getElementById('tryCount');
     this.participants = [];
+    this.addEventListeners();
+  }
+
+  isLengthInRange(str){
+    return str.length <= 5;
+  }
+  isCarNameValid(){
+      const noBlankCarNames = this.$carName.value.replace(/(\s*)/g, "")
+      const carNameList = noBlankCarNames.split(',');
+      carNameList.map(x =>{
+          if(!this.isLengthInRange(x)){
+              alert("자동차 이름을 5자 이하로 입력해주세요")
+          }
+      })
+      console.log(carNameList)
+      
+  }
+  getCarNameList(){
+      return [];
+  }
+  addCarNameSubmitEvent(){
+    this.$carNameSubmit.addEventListener('click', event =>{
+        event.preventDefault();
+        if(this.isCarNameValid()){
+            this.participants = this.getCarNameList();
+        }
+    })
+  }
+  addTryCountSubmitEvent(){
+
+  }
+  addEventListeners(){
+      this.addCarNameSubmitEvent();
+      this.addTryCountSubmitEvent();
   }
 }
 
