5b3bcbf470762d50d7ca3bbb8cbe2cc3d1032659
hanyoonseok <hys6078@naver.com>
2021-12-15 14:23:46 +0900

    docs: README 추가

--- vending/src/index.js
+++
@@ -1,9 +0,0 @@
-import View from './view/index.js';
-import Controller from './controller/index.js';
-
-const init = () => {
-  const view = new View();
-  const controller = new Controller(view);
-};
-
-init();
