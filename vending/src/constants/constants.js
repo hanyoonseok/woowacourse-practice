1ee1965233883bd16df15592c9eea2a5b1c9094f
hanyoonseok <hys6078@naver.com>
2021-12-15 14:32:47 +0900

    feat: 상수 선언 파일 생성

---
+++ vending/src/constants/constants.js
@@ -0,0 +1,44 @@
+export const SELECTOR = {
+  addMenu: 'product-add-menu',
+  vendingMenu: 'vending-machine-manage-menu',
+  purchaseMenu: 'product-purchase-menu',
+
+  productNameInput: 'product-name-input',
+  productPriceInput: 'product-price-input',
+  productQuantityInput: 'product-quantity-input',
+  productAddButon: 'product-add-button',
+  productItem: 'product-manage-item',
+  productName: 'product-manage-name',
+  productPrice: 'product-manage-price',
+  productQuantity: 'product-manage-quantity',
+
+  vendingChargeInput: 'vending-machine-charge-input',
+  vendingChargeButton: 'vending-machine-charge-button',
+  vendingChargeAmount: 'vending-machine-charge-amount',
+  vendingCoin500Quantity: 'vending-machine-coin-500-quantity',
+  vendingCoin100Quantity: 'vending-machine-coin-100-quantity',
+  vendingCoin50Quantity: 'vending-machine-coin-50-quantity',
+  vendingCoin10Quantity: 'vending-machine-coin-10-quantity',
+
+  chargeInput: 'charge-input',
+  chargeButton: 'charge-button',
+  chargeAmount: 'charge-amount',
+  returnButton: 'coin-return-button',
+  coin500Quantity: 'coin-500-quantity',
+  coin100Quantity: 'coin-100-quantity',
+  coin50Quantity: 'coin-50-quantity',
+  coin10Quantity: 'coin-10-quantity',
+  purchaseItem: 'product-purchase-item',
+  purchaseButton: 'purchase-button',
+  purchaseName: 'product-purchase-name',
+  purchasePrice: 'product-purchase-price',
+  purchaseQuantity: 'product-purchase-quantity',
+
+  datasetName: 'data-product-name',
+  datasetPrice: 'data-product-price',
+  datasetQuantity: 'data-product-quantity',
+};
+
+export const ALERT_MESSAGE = {};
+
+export const COIN_ARRAY = [500, 100, 50, 10];
